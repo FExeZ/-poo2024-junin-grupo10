2aa10f0ab8ac514170e209ed7119c51eec084294
Franco <francoexequielzerda@gmail.com>
2024-09-24 18:58:42 -0300

    Primer Commit enviando el formulario de Spring

---
+++ src/main/java/ar/edu/unnoba/proyecto_poo_2024/ProyectoPoo2024Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.proyecto_poo_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPoo2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPoo2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unnoba/proyecto_poo_2024/ProyectoPoo2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.proyecto_poo_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPoo2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
