35a8b0b89672cadcc29007d9c80ad3b2231ae44e
FExeZ <francoexequielzerda@gmail.com>
2024-10-23 16:28:42 -0300

    mismo status

--- src/main/java/ar/edu/unnoba/proyecto_poo_2024/Services/MusicEnthusiastUserService.java
+++ src/main/java/ar/edu/unnoba/proyecto_poo_2024/Services/MusicEnthusiastUserService.java
@@ -1,8 +1,7 @@
 package ar.edu.unnoba.proyecto_poo_2024.Services;
 
 import ar.edu.unnoba.proyecto_poo_2024.Model.MusicEnthusiastUser;
-import ar.edu.unnoba.proyecto_poo_2024.Model.User;
 
 public interface MusicEnthusiastUserService {
-    public void createUser (MusicEnthusiastUser user) throws Exception;
+    public void createUser(MusicEnthusiastUser user) throws Exception;
 }
