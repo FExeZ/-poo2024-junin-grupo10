b2754abcad88b374379e05e133f250d9eca158ad
FExeZ <francoexequielzerda@gmail.com>
2024-11-02 11:26:32 -0300

    verificacion archivos

---
+++ src/main/java/ar/edu/unnoba/proyecto_poo_2024/Controllers/SongController.java
@@ -0,0 +1,44 @@
+package ar.edu.unnoba.proyecto_poo_2024.Controllers;
+
+import org.modelmapper.ModelMapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ar.edu.unnoba.proyecto_poo_2024.Dto.SongResponseDTO;
+import ar.edu.unnoba.proyecto_poo_2024.Model.Song;
+import ar.edu.unnoba.proyecto_poo_2024.Services.AuthorizationService;
+import ar.edu.unnoba.proyecto_poo_2024.Services.SongService;
+
+@RestController
+@RequestMapping("/songs")
+public class SongController {
+
+    @Autowired
+    SongService songService;
+    @Autowired
+    AuthorizationService authorizationService;
+
+    @GetMapping
+    public ResponseEntity<List<SongResponseDTO>> getAllSongs(@RequestHeader("Authorization") String token) {
+        try {
+            authorizationService.authorize(token);
+            List<Song> songs = songService.getAll();
+            ModelMapper modelMapper = new ModelMapper();
+            List<SongResponseDTO> songResponseDTOs = songs.stream()
+                    .map(song -> modelMapper.map(song, SongResponseDTO.class))
+                    .collect(Collectors.toList());
+            return new ResponseEntity<>(songResponseDTOs, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.FORBIDDEN);
+        }
+    }
+}
---
+++ src/main/java/ar/edu/unnoba/proyecto_poo_2024/Dto/SongResponseDTO.java
@@ -0,0 +1,14 @@
+package ar.edu.unnoba.proyecto_poo_2024.Dto;
+
+import ar.edu.unnoba.proyecto_poo_2024.Model.MusicArtistUser;
+import ar.edu.unnoba.proyecto_poo_2024.Model.Enum.Genre;
+import lombok.Data;
+
+@Data
+public class SongResponseDTO {
+    private long id;
+    private String name;
+    private String description;
+    private Genre genre;
+    private MusicArtistUser artist;
+}
